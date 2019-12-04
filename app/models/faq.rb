a09c9fbd475011d5961fdbabc4935b70f044ff13
Andre Macedo <andremacedo.pv@gmail.com>
2019-12-04 17:18:02 -0300

    Agora qnd vc exclui um FAQ ele exclui todas as sugestoes associadas a ele

--- app/models/faq.rb
+++ app/models/faq.rb
@@ -2,4 +2,5 @@ class Faq < ApplicationRecord
   validates :topico, presence: true
   validates :pergunta, presence: true
   validates :resposta, presence: true
+  has_many :faq_sugestion, dependent: :destroy
 end
