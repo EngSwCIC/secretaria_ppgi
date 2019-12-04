8e0d59b3686468bc285fae81167b5c2fb8b44c59
Botelho31 <lucas.vbotelho83@gmail.com>
2019-12-04 19:59:36 -0300

    refazer pedido de credenciamento

--- app/controllers/credenciamento_controller.rb
+++ app/controllers/credenciamento_controller.rb
@@ -10,8 +10,31 @@ class CredenciamentoController < ApplicationController
         end
     end
 
+    def show_form
+        if Credenciamento.where(:iduser => current_user.id).exists?
+            render 'credenciamento/show_recredenciamento'
+        else
+            render 'credenciamento/show_form'
+        end
+    end
+
+    def show_status
+        puts current_user.id
+        if Credenciamento.where(:iduser => current_user.id).exists?
+            render 'credenciamento/show_credenciamento'
+        end
+    end
+
     def create_credenciamento
         Credenciamento.create(iduser: current_user.id)
+        redirect_to '/'
+    end
+
+    def alter_credenciamento
+        @credencial = Credenciamento.find(current_user.id)
+        @credencial.aproved = false
+        @credencial.rejected = false
+        @credencial.save
         redirect_to '/'
     end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,13 @@
 Rails.application.routes.draw do
   get 'home/index'
   get 'credenciamento' => 'credenciamento#show_form'
+  get 'credenciamento/show_status' => 'credenciamento#show_status'
   get 'credenciamento/show_pedidos' => 'credenciamento#show_pedidos'
+  get 'credenciamento/show_recredenciamento' => 'credenciamento#show_recredenciamento'
   get 'credenciamento/aproved' => 'credenciamento#aproved'
   get 'credenciamento/rejected' => 'credenciamento#rejected'
   post 'credenciamento/create_credenciamento' => 'credenciamento#create_credenciamento'
+  post 'credenciamento/alter_credenciamento' => 'credenciamento#alter_credenciamento'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#index'
