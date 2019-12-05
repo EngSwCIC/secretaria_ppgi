5aa6298f7584292449c1ab424a1a5a42a631eae5
evandrotvc <evandro-tvc@hotmail.com>
2019-12-05 20:04:11 -0300

    cucumber e destroy rspec

---
+++ features/specs/cria.feature
@@ -0,0 +1,14 @@
+
+
+#language: pt
+
+Funcionalidade: Inserir os dados no banco 
+
+
+Cenario: Inserção de um novo dados no banco com sucesso
+Dado que acesso o banco e tem-se  informações a inserir
+Quando  insere-se "funcionariooo" e "Proativoo!!" 
+Entao deve inserir com sucesso os dados no banco
+E receber a seguinte mensagem "Attendance was successfully created."
+
+
---
+++ spec/controllers/attendances_controller_spec.rb
@@ -0,0 +1,73 @@
+require 'rails_helper'
+RSpec.feature AttendancesController , type: :controller do
+
+      let(:valid_attributes) {
+    skip("Add a hash of attributes valid for your model")
+  }
+
+  let(:invalid_attributes) {
+    skip("Add a hash of attributes invalid for your model")
+  }
+
+    let(:valid_attributes_search) {
+        {search: "Nada"}
+    }
+    let(:empty_attributes_search) {
+        {search: ""}
+    }
+
+    let(:valid_session) { {} }
+
+
+    describe "GET #index" do
+        it "sucess on index" do
+            get :index
+            expect(response).to have_http_status(:success)
+        end
+    end
+
+    describe "GET #show" do
+    it "returns a success response" do
+      attendance = Attendance.create! valid_attributes
+      get :show, params: {id: attendance.to_param}, session: valid_session
+      expect(notice).to eq('Attendance was successfully created.')
+    end
+  end
+
+    describe "POST #search" do
+        context "with valid params" do
+            it "search was sucessful" do
+                post :search, params: {p: valid_attributes_search}
+                expect(response).to have_http_status(:found)
+            end
+            it "search was invalid" do
+                post :search, params: {p: empty_attributes_search}
+                expect(response).to have_http_status(:found)
+            end
+        end
+    end
+    describe "GET #new" do
+        it "returns a success response" do
+            get :new
+            expect(response).to have_http_status(:success)
+        end
+    end
+
+    describe "DELETE #destroy" do
+    it "destroys the requested datas" do
+        attendance = Attendance.create! valid_attributes
+      expect {
+        delete :destroy, params: {id: attendance.to_param}, session: valid_session
+      }.to change(Attendance, :count).by(-1)
+    end
+
+    it "redirects to the attendances list" do
+      attendance = Attendance.create! valid_attributes
+      delete :destroy, params: {id: attendance.to_params}, session: valid_session
+      expect(response).to redirect_to(attendances_url)
+      
+    end
+  end
+  
+
+end
