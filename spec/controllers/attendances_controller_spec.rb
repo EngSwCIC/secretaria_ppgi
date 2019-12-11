68b901ac53f1d0d6c4dcf81036a6b592d18c9502
Vítor Ribas <bsbribas66@gmail.com>
2019-12-11 20:00:40 -0300

    adiciona testes rspec para attendance controller

--- spec/controllers/attendances_controller_spec.rb
+++ spec/controllers/attendances_controller_spec.rb
@@ -52,4 +52,31 @@ RSpec.feature AttendancesController , type: :controller do
             end
         end
     end
+
+    let(:valid_attributes_search) {
+        {search: "Nada"}
+    }
+    let(:empty_attributes_search) {
+        {search: ""}
+    }
+    #Pagina inicial
+    describe "GET #index" do
+        it "sucess on index" do
+            get :index
+            expect(response).to have_http_status(:success)
+        end
+    end
+    #Pesquisar uma attendance
+    describe "POST #search" do
+        context "with valid params" do
+            it "search was sucessful" do
+                post :search, params: {p: valid_attributes_search}
+                expect(response).to have_http_status(:found)
+            end
+            it "search was invalid" do
+                post :search, params: {p: empty_attributes_search}
+                expect(response).to have_http_status(:found)
+            end
+        end
+    end
 end
