9776c4adf8be8a4abee760f8a68adc7b8867fcc2
afgmlff <aeciofgm@gmail.com>
2020-10-24 17:56:18 -0300

    add aditional files to cucumber/capybara bdd

--- features/support/env.rb
+++ features/support/env.rb
@@ -4,7 +4,16 @@
 # instead of editing this one. Cucumber will automatically load all features/**/*.rb
 # files.
 
+require 'capybara'
+require 'capybara/cucumber'
 require 'cucumber/rails'
+require 'report_builder'
+
+
+Capybara.configure do |config|
+    config.default_driver = :selenium_chrome #roda no navegador
+    #config.default_driver = :selenium_chrome_headless #roda com o nageador em background
+end
 
 # frozen_string_literal: true
 
