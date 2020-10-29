884583975ba1736c92917bd50fe4dbefbbcf7124
MrJamesX37 <47198738+MrJamesX37@users.noreply.github.com>
2020-10-29 12:21:45 -0300

    Adicionado testes unitários da issue #37

--- features/step_definitions/addPersonOnActivite_steps.rb
+++ features/step_definitions/addPersonOnActivite_steps.rb
@@ -1,5 +1,5 @@
 Dado("que estou na página {string}") do |string|
-	redirect_to "localhost:3000/#{string.downcase}" 
+	visit "localhost:3000/#{string.downcase}" 
 end
 
 Quando("eu clico em {string}") do |string|
@@ -7,12 +7,13 @@ Quando("eu clico em {string}") do |string|
 end
 
 Então("eu devo ser redirecionado para {string}") do |string|
-	redirect_to "localhost:3000/#{string.downcase}"
+	visit "localhost:3000/#{string.downcase}"
 end
+
+Quando("preencho o campo {string} com {string}") do |string1, string2|
+	fill_in string1, with: string2
+end	
 
 E("eu devo ver {string}") do |string|
 	expect(page).to have_content string
-end
-
-E("essa pessoa já está cadastrada nessa atividade") do |string|
 end
