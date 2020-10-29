a1c1967437c19f19e8e597b4c04edde74a5add5e
root <root@DESKTOP-ONQ31NF.localdomain>
2020-10-29 20:35:40 -0200

    Implementado cumcumber da feature #9

---
+++ features/step_definitions/Anexar_arquivos.rb
@@ -0,0 +1,27 @@
+Dado('que todos os arquivos anexados são de formatos válidos') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Dado('eu sou secretário') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Quando('eu clicar para anexar arquivos') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Então('os arquivos serão anexados') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Então('mostra uma mensagem de operação concluída') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Dado('que pelo menos um arquivo anexado foi de formato não válido') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Então('mostra uma mensagem de erro') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
