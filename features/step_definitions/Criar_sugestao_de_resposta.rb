c335a4fda427f4028afdbe1ae72e0035aeea1583
brenfeliped <brenofeliped15@live.com>
2020-10-30 19:33:36 -0300

    faq_issues_34_35_sprint1

---
+++ features/step_definitions/Criar_sugestao_de_resposta.rb
@@ -0,0 +1,35 @@
+Given("Que Usuario esteja logado como email {string} e com a senha {string}")_ do |string, string2|
+  visit '/users/sign_in'
+  User.create(full_name: "usuario_teste", email: string, password: string2, role:"estudante", resgistration:"00000000001")
+  fill_in 'user_email', with: string
+  fill_in 'user_password', with: string2
+  click_on 'Log in'
+  page.should have_content("Welcome! You have signed up successfully.")
+  end 
+
+And("Esteja página inicial e logado") do
+    pending
+  end
+
+  And("Clicar no link {link_faq}") do
+    pending
+  end
+
+  When("O Clicar no botão {button_name}") do |button_name|
+    pending
+  end
+
+  Then("O usuário preenche os campos de texto {field} e {value}") do |field, value|
+    pending
+  end
+
+  Then("O usuário clica {button_name}") do |button_name|
+    pending
+  end
+
+  Then("será cadastrado uma nova sugestão de pergunta e resposta") do
+    pending
+  end  
+
+  
+
