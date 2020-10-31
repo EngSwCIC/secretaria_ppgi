c9f3e54450069f1d09ad7adfd7080aac9b12cd43
aliceborges_42 <aliceborges@cjr.org.br>
2020-10-30 23:43:32 -0300

    Faz rspec das controllers

--- spec/controllers/processos_controller_spec.rb
+++ spec/controllers/processos_controller_spec.rb
@@ -2,24 +2,52 @@ require 'rails_helper'
 require 'capybara/rails'
 
 RSpec.describe ProcessosController, type: :controller do
-  before(:each) do
-    @secretario = User.new(email: 'usuario_secretario@secretario.com', password: '123456', is_secretary:true)
-    @secretario.save!
-    sign_in @secretario
+  describe "#create" do
+    it 'calls the create method from the Processos model'
+    describe "valid params" do 
+      it 'saves the process in DB'
+      it "responds with an success HTTP status"
+      it 'redirects to process index page'  
+    end
+    describe "invalid params" do 
+      it "doesn't save the process in DB"
+      it "responds with an error HTTP status"
+    end 
   end
 
-  describe 'GET#show' do
-    it 'deve exibir informações do processo selecionado' do
-      get :show, params: {id: processo.to_param}
-      expect(response).to be_successful
-      expect(response).to have_http_status(200)
+  describe "#update" do 
+    it 'calls the update method from an instance of the Processos model'
+    describe "valid params" do 
+      it 'updates the process data in the DB'
+      it "responds with an success HTTP status"
+      it 'redirects to process index page'  
+    end
+    describe "invalid params" do 
+      it "doesn't update the process data in the DB"
+      it "responds with an error HTTP status"
+    end 
+  end
+
+  describe "#show" do 
+    describe "valid process" do 
+      it "responds with an success HTTP status"
+      it 'redirects to process show page' 
+    end
+    describe "invalid process" do 
+      it "responds with an error HTTP status"
     end
   end
-end
-  describe 'POST #create' do
-    it 'criar um novo processo' do
-      @processo = Processo.create(num_SEI:123, tipo: 1, nome_envolvidos:"Joao, Maria, Luizinho", status: "pending")
-      @processo.should be_an_instante_of Processo
+
+  describe "add comment" do 
+    it "calls the create method from the Comentarios model"
+    describe "valid params" do 
+      it 'saves the comment in DB'
+      it "responds with an success HTTP status"
+      it 'redirects to comment index page'  
     end
+    describe "invalid params" do 
+      it "doesn't save the comment in DB"
+      it "responds with an error HTTP status"
+    end 
   end
 end
