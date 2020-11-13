111d5ed4f6062dff59611c24129cfc1043735ea1
aliceborges_42 <aliceborges@cjr.org.br>
2020-11-13 20:30:41 -0300

    adiciona rspec model comment

--- spec/models/wiki_comment_spec.rb
+++ spec/models/wiki_comment_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe WikiComment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:valid_attributes) {
+    {id: 1, content:'content'}
+  }
+
+  subject(:entry) {
+    WikiComment.create(valid_attributes)
+  }
+
+  describe "Validations" do 
+      
+      it "is valid with the valid attributes" do
+        expect(WikiComment.new(valid_attributes)).to be_valid
+      end
+
+      it "is not valid without a content" do
+        expect(WikiComment.new({id: 1, content: ''})).to_not be_valid
+      end
+      
+  end
+
 end
