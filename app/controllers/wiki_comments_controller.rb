79a765e443c194db48ae2b96816d779219e8c62c
aliceborges_42 <aliceborges@cjr.org.br>
2020-11-12 22:34:33 -0300

    renomeia migrates

--- app/controllers/wiki_comments_controller.rb
+++ app/controllers/wiki_comments_controller.rb
@@ -64,7 +64,7 @@ class WikiCommentsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_comment
-      @comment = Comment.find(params[:id])
+      @comment = WikiComment.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_14_163205) do
+ActiveRecord::Schema.define(version: 2020_11_11_010011) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,21 @@ ActiveRecord::Schema.define(version: 2019_11_14_163205) do
     t.integer "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "wiki_comments", force: :cascade do |t|
+    t.bigint "wiki_entry_id"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["wiki_entry_id"], name: "index_wiki_comments_on_wiki_entry_id"
+  end
+
+  create_table "wiki_entries", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
