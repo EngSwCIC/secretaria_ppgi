99c488b69ec82490d2766a8fe5e722d835ecaa42
Estevam Albuquerque <estevamgalvao1@gmail.com>
2020-11-14 06:15:48 -0300

    all cucumber tests passed

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :activities
+  resources :notifications
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#index'
--- features/step_definitions/notifyInterestedPeople_step.rb
+++ features/step_definitions/notifyInterestedPeople_step.rb
@@ -1,62 +1,31 @@
-#Background
-Dado("que eu estou na página {string}") do |string|
-    visit "localhost:3000/#{string.downcase}"
-end
-
-E("preencho o campo {string} com {string}") do |string1, string2|
-	fill_in string1, with: string2
-end	
-
-E("preencho o campo {string} com {string}") do |string1, string2|
-	fill_in string1, with: string2
-end	
-
-Quando("pressiono {string}") do |string|
-    click_on(string)
-end
-
-Então("eu devo ser redirecionado para {string}") do |string|
-    visit "localhost:3000/#{string.downcase}"
-end
 
 #Cenário feliz
-Dado("que eu estou na página {string}") do |string|
-    visit "localhost:3000/#{string.downcase}"
+Dado("que eu me encontro na página {string}") do |string|
+    visit "/#{string.downcase}"
 end
 
 Quando("pressiono {string}") do |string|
     click_on(string)
 end
 
-Então("eu devo ser redirecionado para {string}") do |string|
-    visit "localhost:3000/#{string.downcase}"
+Então("eu devo estar em {string}") do |string|
+    expect(page).to have_current_path(string)
 end
 
-Quando("preencho o campo {string} com {string}") do |string1, string2|
+Quando("preencho o campo {string} com os {string}") do |string1, string2|
 	fill_in string1, with: string2
 end	
 
-E("pressiono {string}") do |string|
-    click_on(string)
+Quando("no campo {string} eu seleciono {string}") do |string1, string2|
+    select string2, :from => string1
 end
 
-Então("eu devo ver {string}") do |string|
+
+Então("eu devo conseguir ver {string}") do |string|
     expect(page).to have_content string
 end
 
 #Cenário triste
-Dado("que eu estou na página {string}") do |string|
-    visit "localhost:3000/#{string.downcase}"
-end
-
-Quando("pressiono {string}") do |string|
-    click_on(string)
-end
-
-Então("eu devo ser redirecionado para {string}") do |string|
-    visit "localhost:3000/#{string.downcase}"
-end
-
-Então("eu devo ver {string}") do |string|
-    expect(page).to have_content string
+Então("devo ser redirecionado para a página {string}") do |string|
+	visit "/#{string.downcase}"
 end
