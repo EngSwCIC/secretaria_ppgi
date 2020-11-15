574af228a179694c71328f7640f1cf3710c0ee27
Poskvansk <luisfilipesrct@gmail.com>
2020-11-15 15:31:06 -0300

    cansei

--- features/step_definitions/ticket/request_tickets_steps.rb
+++ features/step_definitions/ticket/request_tickets_steps.rb
@@ -65,8 +65,38 @@ E("que eu esteja na página inicial") do
   visit(root_path)
 end
 
-E("eu clicar no botão {string}") do |string|
+E("eu clicar no botão {string}") do |button|
   click_button(button)
+end
+
+Então ('Eu devo estar na página "(.*?)"') do |tickets_index|
+  current_path = URI.parse(current_url).path
+  if current_path.respond_to? :should
+    current_path.should == path_to(tickets_index)
+  else
+    assert_equal path_to(prazos_index), current_path
+  end
+end
+
+E("eu clicar no botão {string}") do |button|
+  click_button(button)
+end
+
+E ('Eu preencho "([^"]*)" com "([^"]*)"') do |field, value|
+  fill_in(field, :with => value)
+end   
+
+Quando ('Eu aperto enter no teclado') do
+  page.evaluate_script('window.confirm = function() { return true; }')
+  page.click('Ok')
+end
+
+Então ('Eu devo ver "([^"]*)"') do |text|
+  if page.respond_to? :should
+    page.should have_content(text)
+  else
+    assert page.has_content?(text)
+  end
 end
 
 Dado("a página de solicitação de passagens seja carregada corretamente") do
