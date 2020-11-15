c2cce15e3c36ee85ff1916c7cfa19334928fb57f
Poskvansk <luisfilipesrct@gmail.com>
2020-11-15 15:12:34 -0300

    a

--- features/step_definitions/ticket/request_tickets_steps.rb
+++ features/step_definitions/ticket/request_tickets_steps.rb
@@ -1,3 +1,54 @@
+# Dado /^Eu esteja cadastrado como usuario com email {string}$/ do |email|
+#     @user = {
+#       Email: email
+#     }
+#     User.create!(@user)
+#   end
+  
+#   E /^Eu esteja autenticado como usuario do
+#     @user = User.find_by_email('user@user.com')
+#   end
+  
+#   E /^Eu esteja na "(.*?)"$/ do |root_path|
+#     visit path_to(root_path)
+#   end
+  
+#   Quando /^Eu clico em ([^"]*)"$/ do |button|
+#     click_button(button)
+#   end
+  
+#   Então /^Eu devo estar na página "(.*?)"$/ do |tickets_index|
+#     current_path = URI.parse(current_url).path
+#     if current_path.respond_to? :should
+#       current_path.should == path_to(prazos_index)
+#     else
+#       assert_equal path_to(prazos_index), current_path
+#     end
+#   end
+  
+#   Quando /^Eu clico em ([^"]*)"$/ do |button|
+#     click_button(button)
+#   end
+  
+#   E /^Eu preencho "([^"]*)" com "([^"]*)"$/ do |field, value|
+#     fill_in(field, :with => value)
+#   end 
+  
+#   Quando /^Eu aperto enter no teclado$/ do
+#     page.evaluate_script('window.confirm = function() { return true; }')
+#     page.click('Ok')
+#   end
+  
+#   Então /^Eu devo ver ([^"]*)"$/ do |text|
+#     if page.respond_to? :should
+#       page.should have_content(text)
+#     else
+#       assert page.has_content?(text)
+#     end
+#   end
+
+######################################################################################
+
 Dado("que eu esteja cadastrado como usuario com email {string}") do |email|
     @email = email
     valid_email = email.eql? "user@user.com"
