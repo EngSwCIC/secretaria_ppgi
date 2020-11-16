b8aafb8f50a92bd5a2a42e1a6e7132abf0d38d43
Joao Viktor <joaoviktor22@hotmail.com>
2020-11-15 21:56:58 -0300

    Implementando RSpec da feature #8

--- spec/models/status_spec.rb
+++ spec/models/status_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe Status, type: :model do
-  it "Válido quando nome esta presente" do
+  it "Valido quando nome esta presente" do
     status = Status.new( name_status: 'Novo')
     expect(status).to be_valid
   end
@@ -11,12 +11,12 @@ describe Status, type: :model do
   it "Invalido sem o nome" do
     status = Status.new( name_status: nil)
     status.valid?
-    expect(status.errors[:name_status]).to include("Não pode deixar espaço de nome em branco.")
+    expect(status.errors[:name_status]).to include("Nao pode deixar espaço de nome em branco.")
   end
 end
 
 describe Status, type: :model do
-  it "é inválido caso já exista um e-mail igual" do
+  it "Invalido caso ja exista um e-mail igual" do
     status = Status.create( name_status: 'Em espera')
     status = Status.new( name_status: 'Em espera')
     status.valid?
