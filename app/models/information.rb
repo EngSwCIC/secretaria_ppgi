d8be1bc2db363dbbca78ade57962bec6358252c1
silvaricardo1997 <silvaricardo1997@gmail.com>
2020-11-18 18:48:56 -0300

    Refactored informations filter

--- app/models/information.rb
+++ app/models/information.rb
@@ -1,4 +1,9 @@
 class Information < ApplicationRecord
     validates :content, uniqueness: true
     validates_presence_of :title, :content, :source_id
+    belongs_to :source
+
+    def self.search(search)
+        where("cast (source_id as text) LIKE ?","%#{search}")
+    end
 end
