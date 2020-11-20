f2c5891ce564aa80a8ac8339bcefd61c135d5272
Gabriel Preihs <gpreihs@gmail.com>
2020-11-20 19:38:58 -0300

    Removing database passwordgit statsu

--- spec/models/requirement_spec.rb
+++ spec/models/requirement_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Requirement, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:valid_attributes) {
+    file = fixture_file_upload(Rails.root.join('public', 'TestImage.png'), 'image/png')
+    {"title" => "test", "content" => "", "documents" => [file]}
+  }
+  it "is valid with valid attributes" do
+    expect(Requirement.new({"title" => "testing"})).to be_valid
+  end
+  it "is not valid without a title"
+  it "is not valid without a description"
+  it "is not valid without a start_date"
+  it "is not valid without a end_date"
 end
