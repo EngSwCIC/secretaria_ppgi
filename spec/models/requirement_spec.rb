56bedf10778843ada189b77d6c3fc848053c5791
Gabriel Preihs <gpreihs@gmail.com>
2020-11-20 20:04:32 -0300

    Requirement specs

--- spec/models/requirement_spec.rb
+++ spec/models/requirement_spec.rb
@@ -1,15 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Requirement, type: :model do
+
   let(:valid_attributes) {
-    file = fixture_file_upload(Rails.root.join('public', 'TestImage.png'), 'image/png')
-    {"title" => "test", "content" => "", "documents" => [file]}
+    {"title" => "testing"}
   }
-  it "is valid with valid attributes" do
-    expect(Requirement.new({"title" => "testing"})).to be_valid
+
+  before(:each) do
+    @admin = User.create!(
+      full_name: "Sower",
+      email: "sower@admin.com",
+      password: "admin123",
+      role: "administrator",
+      registration: "000000000"
+    )
+    sign_in @admin
+    Current.user = @admin
   end
-  it "is not valid without a title"
-  it "is not valid without a description"
-  it "is not valid without a start_date"
-  it "is not valid without a end_date"
+
+  describe "Title validation" do
+    it "is valid with valid attributes" do
+      expect(Requirement.new({"title" => "testing"})).to be_valid
+    end
+
+    it "is not valid without a title" do
+      file = fixture_file_upload(Rails.root.join('public', 'TestImage.png'), 'image/png')
+      expect(Requirement.new({"content" => "", "documents" => [file]})).to_not be_valid
+    end
+
+    it "is not equal to previous requirement title" do
+      requirement = Requirement.create! :valid_attributes
+      expect(Requirement.new({"title" => "testing"})).to_not be_valid
+    end
+  end
+
 end
