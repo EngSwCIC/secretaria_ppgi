4f1b33da3048375aaeffc344405f9cef81d243c7
GustavoMacCar <gmcmacar@gmail.com>
2020-11-23 13:03:39 -0300

    TODO: fix current user template bug

--- app/models/information.rb
+++ app/models/information.rb
@@ -1,7 +1,6 @@
 class Information < ApplicationRecord
     validates :content, uniqueness: true
     validates_presence_of :title, :content, :source_id
-    belongs_to :source
 
     def self.search(search)
         where("cast (source_id as text) LIKE ?","%#{search}")
