37cd6b4249e87425b84f45e19911648f1406a2fd
Manoel <vieiranetoc@gmail.com>
2020-11-23 02:49:44 -0300

    basic proces controller working
    Signed-off-by: Manoel <vieiranetoc@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'process/index'
+  post 'process', to: 'process#create', as: 'process_creation'
+  delete 'process/:id', to: 'process#delete', as: 'process_delete'
   get 'home/index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
