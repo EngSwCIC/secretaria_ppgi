c5c5e7d45962b6399914ef7365416ecd59ed8064
GustavoMacCar <gmcmacar@gmail.com>
2020-11-23 00:03:11 -0300

    notification functionality

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,6 +1,16 @@
 class NotificationsController < ApplicationController
     def index
-      @notifications = Notification.all
+      if current_user.role == 'administrator'
+        @notifications = Notification.all
+      elsif current_user.role == 'secretary'
+        @notifications = Notification.where(interested_group: 'Secretário')
+      elsif current_user.role == 'professor'
+        @notifications = Notification.where(interested_group: 'Professor')
+      elsif current_user.role == 'student'
+        @notifications = Notification.where(interested_group: 'Estudante')
+      end
+
+
     end
     
     def new
@@ -26,14 +36,27 @@ class NotificationsController < ApplicationController
   
       end  
     end
+
+
+    def edit
+      @notification = Notification.find params[:id]
+    end
+    
+    def update
+      @notification = Notification.find params[:id]
+      @notification.update_attributes!(notification_params)
+      flash[:notice] = "Notificação atualizada."
+      redirect_to notification_path(@notification)
+    end
+
+    def show
+      id = params[:id] # retrieve movie ID from URI route
+      @notification = Notification.find(id) # look up movie by unique ID
+    end
   
     private
       def notification_params
         params.require(:notification).permit(:title, :content, :interested_group)
       end
-  
-    def show
-        @notification=Notification.find(params[:id])
-    end
   end
   
