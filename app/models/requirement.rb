920e3e682ab83194af7200307b28f8e1bfa91685
Gabriel Preihs <gpreihs@gmail.com>
2020-12-04 18:17:19 -0300

    Adding hashs to model

--- app/models/requirement.rb
+++ app/models/requirement.rb
@@ -6,11 +6,14 @@ class Requirement < ApplicationRecord
     validates :title, presence: true, uniqueness: true
     has_many_attached :documents
 
-    validate :check_role, on: [:create, :update]
+    ##
     # Define se usuário atual está logado e se é administrador do sistema
+    validate :check_role, on: [:create, :update]
     def current_user_is_admin
         Current.user != nil && Current.user.role == 'administrator'
     end
+
+    ##
     # Permite criação ou atualização de requisitos por um administrador
     def check_role
         unless current_user_is_admin
@@ -27,6 +30,8 @@ class Requirement < ApplicationRecord
     def allow_deletion!
         @allow_deletion = true
     end
+
+    ##
     # Permite deleção de requisitos por um administrador ou caso metodo 'allow_deletion!' tenha sido chamado pela instancia
     def check_permission
         unless @allow_deletion || current_user_is_admin
