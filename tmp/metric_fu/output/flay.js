def97f73b80d868393fe5fe25cc52955c7e4d6be
GustavoMacCar <gmcmacar@gmail.com>
2020-12-04 20:48:50 -0300

    add rubycritic

---
+++ tmp/metric_fu/output/flay.js
@@ -0,0 +1,3 @@
+        var graph_title = 'Flay: duplication';
+        var graph_series = [{name: 'flay', data: [970]}];
+        var graph_labels = {"0":"11/25"};
---
+++ tmp/metric_fu/output/stats.js
@@ -0,0 +1,3 @@
+        var graph_title = 'Stats: LOC & LOT';
+        var graph_series = [{name: 'LOC', data: [186]},{name: 'LOT', data: [218]}];
+        var graph_labels = {"0":"11/25"};
