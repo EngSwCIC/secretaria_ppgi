2fc0cc61b7fc0eb4b4b686f505781608fb7d45f2
brenfeliped <brenofeliped15@live.com>
2020-12-04 22:27:22 -0300

    clean controllers

--- app/controllers/faqs_controller.rb
+++ app/controllers/faqs_controller.rb
@@ -62,12 +62,12 @@ class FaqsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+   
     def set_faq
       @faq = Faq.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+   
     def faq_params
       params.require(:faq).permit(:question, :answer, :topic_id)
     end
--- app/controllers/faqs_suggestions_controller.rb
+++ app/controllers/faqs_suggestions_controller.rb
@@ -1,28 +1,24 @@
 class FaqsSuggestionsController < ApplicationController
   before_action :set_faqs_suggestion, only: [:show, :edit, :update, :destroy]
 
-  # GET /faqs_suggestions
-  # GET /faqs_suggestions.json
+ 
   def index
     @faqs_suggestions = FaqsSuggestion.all
   end
 
-  # GET /faqs_suggestions/1
-  # GET /faqs_suggestions/1.json
+  
   def show
   end
 
-  # GET /faqs_suggestions/new
+  
   def new
     @faqs_suggestion = FaqsSuggestion.new
   end
 
-  # GET /faqs_suggestions/1/edit
   def edit
   end
 
-  # POST /faqs_suggestions
-  # POST /faqs_suggestions.json
+  
   def create
     @faqs_suggestion = FaqsSuggestion.new(faqs_suggestion_params)
 
@@ -37,8 +33,7 @@ class FaqsSuggestionsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /faqs_suggestions/1
-  # PATCH/PUT /faqs_suggestions/1.json
+ 
   def update
     respond_to do |format|
       if @faqs_suggestion.update(faqs_suggestion_params)
@@ -51,8 +46,6 @@ class FaqsSuggestionsController < ApplicationController
     end
   end
 
-  # DELETE /faqs_suggestions/1
-  # DELETE /faqs_suggestions/1.json
   def destroy
     @faqs_suggestion.destroy
     respond_to do |format|
@@ -62,12 +55,12 @@ class FaqsSuggestionsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+    
     def set_faqs_suggestion
       @faqs_suggestion = FaqsSuggestion.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+   
     def faqs_suggestion_params
       params.require(:faqs_suggestion).permit(:question, :answer, :topic_id)
     end
