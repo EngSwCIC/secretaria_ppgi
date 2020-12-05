0974be9b11058e18e14a5a943dc489e7aecae43e
Aline Ávila <aline.avila01@gmail.com>
2020-12-05 01:03:10 +0000

    Adicionado rspec para topics

--- spec/views/faqs/edit.html.erb_spec.rb
+++ spec/views/faqs/edit.html.erb_spec.rb
@@ -22,3 +22,26 @@ RSpec.describe "faqs/edit", type: :view do
     end
   end
 end
+
+RSpec.describe "topics/edit", type: :view do
+  before(:each) do
+    @faq = assign(:faq, Faq.create!(
+      :question => "MyString",
+      :answer => "MyString",
+      :topic => "MyString"
+    ))
+  end
+
+  it "renders the edit faq form" do
+    render
+
+    assert_select "form[action=?][method=?]", faq_path(@faq), "post" do
+
+      assert_select "input[name=?]", "faq[question]"
+
+      assert_select "input[name=?]", "faq[answer]"
+
+      assert_select "input[name=?]", "faq[topic_id]"
+    end
+  end
+end
--- spec/views/faqs/show.html.erb_spec.rb
+++ spec/views/faqs/show.html.erb_spec.rb
@@ -16,3 +16,21 @@ RSpec.describe "faqs/show", type: :view do
     expect(rendered).to match(//)
   end
 end
+
+RSpec.describe "topics/show", type: :view do
+  before(:each) do
+    @faq = assign(:faq, Faq.create!(
+      :question => "Question",
+      :answer => "Answer",
+      :topic => "Topic"
+    ))
+  end
+
+  it "renders attributes in <p>" do
+    render
+    expect(rendered).to match(/Question/)
+    expect(rendered).to match(/Answer/)
+    expect(rendered).to match(/Topic/)
+  end
+end
+
